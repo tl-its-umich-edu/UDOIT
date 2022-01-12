552a70c8b3d60f77be6010b6d7302a5725a206bd
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2022-01-12 17:59:20 -0500

    Adding an option to run migrations or not

--- umich-start.sh
+++ umich-start.sh
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env bash
 
 # https://documentation.its.umich.edu/node/2118
@@ -8,12 +7,14 @@ if [ "$(id -u)" -ge 1000 ] ; then
     rm /tmp/passwd
 fi
 
-# Run migrations
-echo "Waiting for DB"
-while ! php bin/console dbal:run-sql 'SELECT version()' > /dev/null 2>&1; do   
-  echo "Waiting 1 second for database to be available."
-  sleep 1 # wait 1 second before check again
-done
+if [ "${RUN_MIGRATIONS}" = true ] ; then
+    # Run migrations
+    echo "Waiting for DB"
+    while ! php bin/console dbal:run-sql 'SELECT version()' > /dev/null 2>&1; do   
+        echo "Waiting 1 second for database to be available."
+        sleep 1 # wait 1 second before check again
+    done
+fi
 
 php bin/console doctrine:migrations:migrate --no-interaction
 
